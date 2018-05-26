11e3da91ced8cb5f186c15e41d5df7a945fc7b5d
moltres <25852469@qq.com>
2018-05-26 08:47:04 +0800

    cook2

--- Assets/Scripts/br/Cook3Manager.cs
+++ Assets/Scripts/br/Cook3Manager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MyBehaviour {
+public class Cook3Manager : MyBehaviour {
 
 	public GameObject hooksP;
 	public GameObject[] hooks;
