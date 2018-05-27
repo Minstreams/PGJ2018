aa0cfa98e95533a77ae1a0211a86fbf7b9026cb6
chiellini <18319277690@163.com>
2018-05-27 13:24:09 +0800

    fire prefab组件五秒之后自动消失

---
+++ Assets/Cook3FireDestory.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cook3FireDestory : MyBehaviour {
+
+	
+	void Start () {
+        StartCoroutine(DestoryMySelf());
+	}
+	
+    IEnumerator DestoryMySelf()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+    }
+}
